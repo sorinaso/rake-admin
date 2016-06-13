9cbbe01003db7bda6da0f8cd0471086b256735f3
Alejandro Souto <sorinaso@gmail.com>
2016-06-12 21:51:46 -0300

    Actualizado docker

--- lib/rake/admin/docker.rb
+++ lib/rake/admin/docker.rb
@@ -3,36 +3,72 @@ require 'rake/admin/validations'
 
 module Rake
   module Admin
-    module DockerImage
-      class Local < Rake::TaskLib
-        attr_accessor :tag, :path, :repository
+    module Docker
+      class Image < Rake::TaskLib
+        attr_accessor :name, :dockerfile_path
 
         include Rake::Admin::Validations
 
         def initialize
           yield self if block_given?
 
-          validate_not_nil(@repository, "must define repository parameter")
-          validate_not_nil(@tag, "must define tag parameter")
-          validate_not_nil(@path,"must define path parameter")
-
-          @repository_and_tag = "#{@repository}:#{@tag}"
+          validate_not_nil(@name, "must define the name parameter")
+          validate_not_nil(@dockerfile_path,"must define dockerfile_path parameter")
 
           define
         end
 
         def define
 
-          desc "Build the docker image with the tag `#{@repository_and_tag}` from Dockerfile in path `#{@path}`"
+          desc "Build the docker image with the tag `#{@name}` from Dockerfile in path `#{@dockerfile_path}`"
           task :build do
-            sh "docker build -t #{@repository_and_tag} #{@path}"
-          end
-
-          desc "Run a container with a terminal from the docker image `#{@repository_and_tag}`"
-          task :run_container do
-            sh "docker run -it #{@repository_and_tag}"
+            sh "docker build -t #{@name} #{@dockerfile_path}"
           end
         end
+      end
+
+      class Container < Rake::TaskLib
+
+        attr_accessor :name, :image_name, :run_paramaters, :run_command
+
+        include Rake::Admin::Validations
+
+        def initialize
+          yield self if block_given?
+
+          validate_not_nil(@name, "must define the name parameter")
+          validate_not_nil(@image_name,"must define image_name parameter")
+
+          @run_parameters ||= ''
+
+          @run_command ||= ''
+
+          define
+        end
+
+        def define
+
+          desc "Inicia el container #{@name}"
+          task :start do
+            sh "docker start '#{@name}'"
+          end
+
+          desc "Se atachea al container #{@name}"
+          task :attach do
+            sh "docker attach '#{@name}'"
+          end
+
+          desc "Borra el container #{@name}"
+          task :delete do
+            sh "docker rm -f '#{@name}'"
+          end
+
+          desc "Abre un shell en un container de docker."
+          task :run do
+            sh "docker run #{@run_parameters} --name '#{@name}' #{@image_name} #{@run_command}"
+          end
+        end
+
       end
     end
   end
